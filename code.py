c4bc31b5110942fb87a75169c021b8ba3badcce5
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2024-01-09 12:13:19 -0500

    Done

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created By: Julien Lamoureux
+# Date: January 9, 2024
+# This is the code for the pybadge game "Space Aliens"
+
+import ugame
+import stage
+
+def game_scene():
+    # this function is the main game scene
+    
+    # display Hello Julien
+    print("Hello Julien!")
+
+    # use a whileTrue loop to keep the game running
+    whileTrue :
+        pass
+
+if__name__== "__main__":
+    game_scene()
