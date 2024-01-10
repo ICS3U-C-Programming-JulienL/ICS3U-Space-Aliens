a394d6ec88d268eeb5d433f9a22cd272d32d91b0
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2024-01-10 12:08:04 -0500

    Done

--- code.py
+++ code.py
@@ -11,18 +11,32 @@ import ugame
 def game_scene():
     # this function is the main game scene
 
-    # declare background variable
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_background")
+    # declare image_bank_background variable
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    # declare image_bank_sprites 
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # declare ship
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
+    # declare background
     background = stage.Grid(image_bank_background, 10, 8)
 
-    # declare game
+    # declare game, game layers, and render_block
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship]+ [background]
     game.render_block()
 
     # use a whileTrue loop to keep the game running
     while True:
-        pass
+        # get user input
+
+        # update game logic
+
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
