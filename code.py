efcd5021665b8880013d6f4af3d9eb0cf454d6be
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2024-01-12 12:33:49 -0500

    Done CPT 5

--- code.py
+++ code.py
@@ -4,10 +4,9 @@
 # Date: January 9, 2024
 # This is the code for the pybadge game "Space Aliens"
 
+import constants
 import stage
 import ugame
-
-import constants
 
 
 def game_scene():
@@ -19,43 +18,17 @@ def game_scene():
     # declare image_bank_sprites
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # buttons that need state information
-    a_button = constants.button_state["button_up"]
-    b_button = constants.button_state["button_up"]
-    start_button = constants.button_state["button_up"]
-    select_button = constants.button_state["button_up"]
-
-    # prepare sound
-    pew_sound = open("pew.wav", "rb")
-    sound = ugame.audio
-    sound.stop()
-    sound.mute(False)
-
     # declare ship
-    ship = stage.Sprite(
-        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
-    )
-
-    # declare alien
-    alien = stage.Sprite(
-        image_bank_sprites,
-        9,
-        int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
-        16,
-    )
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # declare background
     background = stage.Grid(
         image_bank_background, constants.SCREEN_X, constants.SCREEN_Y
     )
 
-    # declare game and set FPS
+    # declare game, game layers, and render_block
     game = stage.Stage(ugame.display, constants.FPS)
-
-    # set the game layers
-    game.layers = [ship] + [alien] + [background]
-
-    # render background and sprite list location
+    game.layers = [ship] + [background]
     game.render_block()
 
     # use a whileTrue loop to keep the game running
@@ -63,32 +36,20 @@ def game_scene():
         # get user input
         keys = ugame.buttons.get_pressed()
 
-        # A button status check
-        if keys & ugame.K_O != 0:
-            # if the a button is up, make it so it was just pressed
-            if a_button == constants.button_state["button_up"]:
-                a_button = constants.button_state["button_just_pressed"]
-            elif a_button == constants.button_state["button_just_pressed"]:
-                # otherwise, if the a button was just pressed, make it so it is still pressed
-                a_button = constants.button_state["button_still_pressed"]
-        else:
-            # otherwise, if the a button is still pressed, make it so it was just released
-            if a_button == constants.button_state["button_still_pressed"]:
-                a_button = constants.button_state["button_released"]
-            else:
-                # otherwise, make it so the a button is up
-                a_button = constants.button_state["button_up"]
-
         # if B is pressed, then pass
-        if keys & ugame.K_X != 0:
+        if keys & ugame.K_X:
+            pass
+
+        # if A is pressed, then pass
+        if keys & ugame.K_O:
             pass
 
         # if Start is pressed, then pass
-        if keys & ugame.K_START != 0:
+        if keys & ugame.K_START:
             pass
 
         # if Select is pressed, then pass
-        if keys & ugame.K_SELECT != 0:
+        if keys & ugame.K_SELECT:
             pass
 
         # if right on the d-pad is pressed, move the ship right
@@ -110,19 +71,16 @@ def game_scene():
                 ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
 
         # if up on the d-pad is pressed,then pass
-        if keys & ugame.K_UP != 0:
+        if keys & ugame.K_UP:
             pass
 
         # if down on the d-pad is pressed, then pass
-        if keys & ugame.K_DOWN != 0:
+        if keys & ugame.K_DOWN:
             pass
         # update game logic
-        # if the A button was just pressed . play pew sound effect
-        if a_button == constants.button_state["button_just_pressed"]:
-            sound.play(pew_sound)
 
         # redraw Sprites
-        game.render_sprites([ship] + [alien])
+        game.render_sprites([ship])
         game.tick()
 
 
